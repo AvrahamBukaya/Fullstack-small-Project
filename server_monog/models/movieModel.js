3d58e0af67a6883005059538ab7396e60e1c9e8f
avrahambukaya@gmail.com <100492281+AvrahamBukaya@users.noreply.github.com>
2023-01-02 22:58:53 +0200

    'second_commit'

--- server_monog/models/movieModel.js
+++ server_monog/models/movieModel.js
@@ -4,6 +4,7 @@ const movieSchema = new mongoose.Schema({
     name: String,
     director: String,
     premieredYear: Number,
+    img: String,
 }, { versionKey: false });
 
 const Movie = mongoose.model('movies', movieSchema);
